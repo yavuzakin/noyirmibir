cb8230201e45f2275bd779e40078947c8779b9c4
yavuzakinn <17050111035@ybu.edu.tr>
2022-06-22 16:06:42 +0300

    hero section is made responsive

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,4 +1,6 @@
 import styles from "./Hero.module.scss";
+import hero from "../../assets/images/hero.png";
+import heroMobile from "../../assets/images/hero-mobile.png";
 
 interface Props {
   headerText: string;
@@ -8,6 +10,11 @@ interface Props {
 const Hero = (props: Props) => {
   return (
     <section className={styles["hero"]}>
+      <picture>
+        <source srcSet={heroMobile} media="(max-width: 46.875em)" />
+        <source srcSet={hero} media="(min-width: 46.875em)" />
+        <img src={hero} className={styles["hero__image"]} />
+      </picture>
       <div className={styles["hero__box"]}>
         <h1 className={styles["hero__header"]}>{props.headerText}</h1>
         <button className={styles["hero__btn"]}>{props.btnText}</button>
